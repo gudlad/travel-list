67742efc70d484cb1fe5ef9f23c071260075f8c7
gudladd <19a31@sdmit.in>
2023-09-23 13:14:20 +0530

    initial changes

---
+++ src/Components/App.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Stats from "./Stats";
+import PackingList from "./PackingList";
+import Form from "./Form";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    // we can't directly modify the items like items=> items.push(item) this is wrong
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearItems() {
+    const confirm = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirm) {
+      setItems([]);
+    }
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItems={handleDeleteItems}
+        onToggleItem={handleToggleItem}
+        onClearItems={handleClearItems}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
